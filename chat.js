b150c79f7acb4a505c2f560c68553a3fac9f0b4c
BRUNO PIERRE COPERCINI DE OLIVEIRA <bruno.copercini.oliveira@escola.pr.gov.br>
2026-05-19 13:48:56 +0000

    a

--- chat.js
+++ chat.js
@@ -1,3 +1,44 @@
+// 1. Configuração do Firebase (Cole AS SUAS CHAVES aqui)
+const firebaseConfig = {
+  apiKey: "AIzaSyBTp8oCK8qI96g7ru4Ot69v9Zh8Q01jRZk",
+  authDomain: "appclicker67.firebaseapp.com",
+  projectId: "appclicker67",
+  storageBucket: "appclicker67.firebasestorage.app",
+  messagingSenderId: "747324004819",
+  appId: "1:747324004819:web:187548329a3e35d5127726",
+};
+
+// 2. Inicializando os serviços
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// 3. Mapeando os novos elementos do DOM
+const telaLogin = document.getElementById("tela-login");
+const chatContainer = document.getElementById("chat-container");
+const btnLogin = document.getElementById("btn-login");
+
+// Variável global para guardar os dados de quem logou
+let usuarioAtual = null;
+
+// 4. Função de Login com Google
+btnLogin.onclick = function () {
+  const provedorGoogle = new firebase.auth.GoogleAuthProvider();
+
+  auth
+    .signInWithPopup(provedorGoogle)
+    .then(function (resultado) {
+      // Deu certo! Guarda os dados do usuário
+      usuarioAtual = resultado.user;
+      console.log("Logado como:", usuarioAtual.displayName);
+
+      // Esconde o Login e Mostra o Chat
+      telaLogin.style.display = "none";
+      chatContainer.style.display = "flex";
+    })
+    .catch(function (erro) {
+      alert("Erro ao fazer login: " + erro.message);
+    });
+};
 // 1. Mapeando o DOM
 const areaMensagens = document.getElementById("area-mensagens");
 const inputTexto = document.getElementById("input-texto");
